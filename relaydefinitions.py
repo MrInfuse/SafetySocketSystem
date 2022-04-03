e91e711ce042605726c9b5bc8d22894fae65dddb
MrInfuse <tjohndereck@gmail.com>
2022-04-03 08:42:34 +0800

    add relay control

---
+++ relaydefinitions.py
@@ -0,0 +1,21 @@
+relays = [
+    { 'id' : 1, 'name' : 'SOCKET 01', 'state' : 'off'},
+    { 'id' : 2, 'name' : 'SOCKET 02', 'state' : 'off'},
+    { 'id' : 3, 'name' : 'SOCKET 03', 'state' : 'off'},
+    { 'id' : 4, 'name' : 'SOCKET 04', 'state' : 'off'},
+    { 'id' : 5, 'name' : 'SOCKET 05', 'state' : 'off'},
+    { 'id' : 6, 'name' : 'SOCKET 06', 'state' : 'off'},
+    { 'id' : 7, 'name' : 'SOCKET 07', 'state' : 'off'},
+    { 'id' : 8, 'name' : 'SOCKET 08', 'state' : 'off'}
+    ]
+
+relayIdToPin = {
+    1 : 65,
+    2 : 66,
+    3 : 67,
+    4 : 68,
+    5 : 69,
+    6 : 70,
+    7 : 71,
+    8 : 72
+    }
