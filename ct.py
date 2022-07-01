60d26126374e8fe49c8448525c8f7949ece38898
MrInfuse <39250321+MrInfuse@users.noreply.github.com>
2022-07-01 13:34:56 +0800

    Delete ct.py

--- ct.py
+++
@@ -1,6 +0,0 @@
-import serial
-from flask import Blueprint, request, url_for, render_template
-
-currentSensor = Blueprint("currentSensor", __name__, static_folder="static", template_folder="templates")
-
-
